b231c83c48d96f8eded5578134eba614159c1b39
Adam Coogan <acoogan@ucsc.edu>
2015-10-08 17:52:58 -0700

    Ported ipython notebooks to python scripts since jupyter does not work correctly with OS X El Capitan.

---
+++ NewSolarProp/scripts/AnisotropyCalc.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+
+"""
+Modulates energy spectrum using results from propagation code.
+"""
+
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+import subprocess
+import os.path
+from os import listdir
+from os.path import isfile, join
+
+rundataPath = 'rundata/'
+generatorDir = 'bin/'
+generator = 'StraussUnits'
+
+"""
+Basic constants and functions related to propagation quantities.
+"""
+
+# Solar cycle polarity
+Ac = -1
+# Magnetic field
+rRefB = 1.0
+rSun = 0.005 # AU
+B0 = 5e-9 / np.sqrt(1 + (rRefB - rSun)**2) * 4.485e10 # GV / AU
+Omega = 2.0 * np.pi / (25.4 * 24.0 * 3600.0) # rad / s
+Vsw = 400 * 6.685e-9 # AU / s
+# Electron mass
+me = 0.000511
+qe = -1
+# Diffusion parameters
+lambda0 = 0.15 # AU
+kperp_kpar = 0.01
+
+def momentum(ek):
+    return np.sqrt(ek * (ek + 2.0 * me)) # GeV
+
+def velocity(ek):
+    return 0.002 * np.sqrt(ek * (ek + 2.0 * me)) / (ek + me) # AU / s
+
+def rigidity(ek):
+    return np.sqrt(ek * (ek + 2.0 * me)) # GV
+
+def getB(r, th, ph):    
+    bFact = Ac * B0 * (rRefB / r)**2
+    return {bFact, 0.0, -bFact * (r - rSun) * Omega / Vsw * np.sin(th)}
+
+def getK(r, th, ph, ek):
+    # Diffusion tensor
+    k = {'rr': 0.0, 'thth': 0.0, 'phph': 0.0, 'rth': 0.0, 'rph': 0.0, 'thr': 0.0, 'thph': 0.0, 'phr': 0.0, \
+            'phth': 0.0}
+
+    # Set up convenience variables
+    p = momentum(ek)
+    v = velocity(ek)
+    B = getB(r, th, ph, -1)
+
+    # Asymmetric part of k in spherical coordinates
+    kAFact = p * v / (3 * qe * (B['r']**2 + B['ph']**2))
+    k['rth'] = kAFact * B['ph']
+    k['thr'] = -kAFact * B['ph']
+    k['thph'] = kAFact * B['r']
+    k['phth'] = -kAFact * B['r']
+
+    # Symmetric part of k
+    kpar = v / 3 * lambda0 * (1 + r / rRefLambda)
+    if p > p0:
+        kpar = kpar * p / p0
+        kperp = kperp_kpar * kpar
+
+    # Convert symmetric part to spherical coordinates
+    tanPsi = Omega * (r - rSun) * np.sin(th) / Vsw
+    cosPsi = 1 / np.sqrt(1 + tanPsi**2)
+    sinPsi = np.sqrt(1 - cosPsi**2)
+
+    k['rr'] = kpar * cosPsi**2 + kperp * sinPsi**2
+    k['phph'] = kpar * sinPsi**2 + kperp * cosPsi**2
+    k['thth'] = kperp
+    k['rph'] = k['rph'] + (kperp - kpar) * cosPsi * sinPsi
+    k['phr'] = k['phr'] + (kperp - kpar) * cosPsi * sinPsi
+
+    return k
+
+"""
+Local interstellar spectrum.
+"""
+
+def jLISLangner(ek):
+    """
+    LIS parametrization from Langner 2004, with correction from Strauss' code.  ek is in GeV.
+    Returns: j_LIS in units of particles m^-2 s^-1 sr^-1 MeV^-1.
+    """
+    P = rigidity(ek) / 1.0 # P0 = 1.0 GV
+
+    if P < 0.0026:
+        a = 126.067948848145
+        b = 0.2567973348983205
+        c = 1.95129069032843
+        d = 0.0171199701826333
+
+        return 1.7 * (a + c * np.log(P)) / (1.0 + b * np.log(P) + d * np.log(P)**2)
+    elif P >= 0.0026 and P < 0.1:
+        return 1.7 * ((52.55 + 23.01 * P) / (1.0 + 148.62 * P))**2
+    elif P >= 0.1 and P <= 10.0:
+        return (1555.89 + 17.36 * P - 3.4e-3 * P**2 + 5.13e-7 * P**3)/(1.0 - 11.22 * P + 7532.93 * P**2
+                + 2405.01 * P**3 + 103.87 * P**4)
+    elif P > 10.0:
+        return 1.7 * np.exp(-0.89 - 3.22 * np.log(P))
+
+def getJ(runName):
+    """
+    Performs MC integration to find the differential intensity at earth for a given run.
+    runName: csv containing trajectory exit points.
+    Returns:
+    intitialPoint: the point at which the cosmic rays were observed and their energy.
+    j: the differential intensity at that point and energy.
+    sigma: the error on j.
+    """
+    # Load exit points
+    rawData = np.loadtxt(rundataPath + runName + '.csv', delimiter = ',')
+    # Parse out the coordinates and energies into a list of tuples of exit points
+    exitData = [{'ee': ee, 'the': the, 'phe': phe} for ee, the, phe \
+            in zip(rawData[:,3], rawData[:,1], rawData[:,2])]
+
+    # Get initial point.  --->>> TODO: store initial point in first line of csv!!!
+    r0 = 1.0
+    th0 = np.pi / 2.0
+    ph0 = 0.0
+    e0 = 0.1
+    initialPoint = {'r0': r0, 'th0': th0, 'ph0': ph0, 'e0': e0}
+
+    # Average LIS at exit points
+    #print([jLIS(ep['ee']) for ep in exitData][:10])
+    jN = sum([jLISLangner(ep['ee']) / rigidity(ep['ee'])**2 for ep in exitData]) \
+            / float(len(exitData)) * rigidity(e0)**2
+
+    # Compute error
+    varjN = sum([(jLISLangner(ep['ee']) * (rigidity(e0) / rigidity(ep['ee']))**2 - jN)**2 for ep in exitData])
+    varjN = varjN * 1.0 / (len(exitData) - 1.0)
+
+    return (initialPoint, jN, np.sqrt(varjN / len(exitData)))
+
+#############################################################
+
+x0, j, sigma = getJ('hyades/alt0_0.1GeV')
+print('e0 = ' + str(x0['e0']) + ' GeV, j(x0) = ' + str(round(j, 5)) + ' +/- ' + str(round(sigma, 5))
+        + ' MeV^-1 s^-1 sr^-2 m^-2')
+
+x0, j, sigma = getJ('macbookpro/precision_test/strauss_alt0_runs')
+print('e0 = ' + str(x0['e0']) + ' GeV, j(x0) = ' + str(round(j, 5)) + ' +/- ' + str(round(sigma, 5))
+        + ' MeV^-1 s^-1 sr^-2 m^-2')
+
+
--- NewSolarProp/scripts/ExitStats.py
+++ NewSolarProp/scripts/ExitStats.py
@@ -4,145 +4,79 @@
 Functions for performing statistical analyses dependent only on first and last points in MCs
 """
 
-import SolarPropContainers
-import matplotlib
 import numpy as np
+import scipy
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import xml.etree.ElementTree as ET
-import os, sys
+import itertools
 
-## Path to root directory for MC runs
-#mcPath = sys.argv[1]
+# Load exit data
+runDir = 'rundata/'
+#fName = 'hyades/alt0_0.1GeV'
+#fName = 'strauss_run5'
+#fName = 'straussunits_alt0_run5'
+fName = 'macbookpro/precision_test/strauss_alt0_runs'
 
-###
-# Parses all XML files in the specified directory.  Recursively traverses subdirectories
-# Arguments:
-# Returns:
-###
-def parseTrajs(mcPath):
-    # Store trajectories
-    trajectories = []
+rawData = np.loadtxt(runDir + fName + '.csv', delimiter = ',')
 
-    # Traverse the directories in mcPath to find all XML files in the tree
-    for (dirPath, dirNames, fileNames) in os.walk(mcPath):
-        for fileName in fileNames:
-            # Parse each XML file
-            if (len(fileName) > 4 and fileName[-4:] == ".xml"):
-                traj = parseTraj(os.path.join(dirPath, fileName))
-                
-                # Make sure the trajectory didn't hit the sun
-                if traj != None:
-                    trajectories.append(parseTraj(os.path.join(dirPath, fileName)))
-    
-    return trajectories
+# Parse out the coordinates, energies and times
+exitData = {'r': rawData[:,0], \
+        'th': 180.0 / np.pi * rawData[:,1], \
+        'ph': 180.0 / np.pi * rawData[:,2], \
+        'ek': rawData[:,3], \
+        's': rawData[:,4] / (24.0 * 3600.0)}
 
-###
-# Parses a SolarProp trajectory.
-# Arguments:
-#   trajPath: the XML file"s path.
-# Returns: a MinTrajectory containing the trajectory"s first and last points.
-###
-def parseTraj(trajPath):
-    # Load up the XML
-    root = ET.parse(trajPath).getroot()
 
-    if root.find("boundary").text.strip() == "heliopause":
-        # Put points into a list.  Get the first and last elements.
-        points = root.find("points").findall("point")
-        first = SolarPropContainers.PPoint(float(points[0].find("r").text), float(points[0].find("th").text),
-                float(points[0].find("phi").text), float(points[0].find("e").text),
-                float(points[0].find("t").text))
+# Toggle log scale on the theta and energy plots' y axes
+logScale = True
 
-        last = SolarPropContainers.PPoint(float(points[-1].find("r").text), float(points[-1].find("th").text),
-                float(points[-1].find("phi").text), float(points[-1].find("e").text),
-                float(points[-1].find("t").text))
+# Create figure for plotting polar angle, energy and time at exit
+fig = plt.figure(figsize = (16, 6))
+thAx = fig.add_subplot(1, 3, 1)
+ekAx = fig.add_subplot(1, 3, 2)
+sAx = fig.add_subplot(1, 3, 3)
 
-        return SolarPropContainers.MinTrajectory(first, last)
-    else:
-        return None
+# Fill the histograms
+thData = []
+sData = []
+ekData = []
+for i in range(len(exitData['th'])):
+    thData.append(exitData['th'][i])
+    sData.append(exitData['s'][i])
+    ekData.append(exitData['ek'][i])
 
-if __name__ == "__main__":
-    # Path to root directory for MC runs
-    mcPath = sys.argv[1]
+# Get expected propagation time
+propTime = sum(exitData['s']) / float(len(exitData['s']))
+propTimeError = sum([(s - propTime)**2 for s in exitData['s']]) / (len(exitData['s']) - 1)
+propTimeError = np.sqrt(propTimeError / float(len(exitData['s'])))
+#print('Expected propagation time: ' + str(propTime) + ' +/- ' + str(propTimeError) + ' days')
+#print('Should be ~300 days for Ac > 0, ~180 days for Ac < 0')
 
-    # Load all the trajectories
-    trajectories = parseTrajs(mcPath)
+# Fill the histograms
+thAx.hist(thData, bins = np.arange(0.0, 180.0 + 6.0, 6.0), log = logScale)
+ekAx.hist(ekData, bins = np.arange(0.0, 3.0 + 0.1, 0.1), log = logScale)
+sMin = 0.0
+sMax = 1000.0
+sWidth = 10.0
+sAx.hist(sData, bins = np.arange(sMin, sMax, sWidth))
 
-    print("Analyzing " + str(len(trajectories)) + " MC trajectories...")
+# Scale axes
+axScale = len(exitData['th']) / 10000.0
 
-    # Count the number of galactic particles
-    fromGalaxy = 0
-    # Store exit energies, polar angles and propagation times
-    thBelow = []
-    thAbove = []
-    eBelow = []
-    eAbove = []
-    tBelow = []
-    tAbove = []
+# Set up axes and labels
+thAx.set_ylim(0.9, axScale*6000.0)
+ekAx.set_ylim(0.9, axScale*2500.0)
+sAx.set_ylim(0.9, axScale*650.0) #(700.0 if fName[1:3] == 'lt' else 500.0))
+thAx.set_ylabel('Number of occurences')
+ekAx.set_xlim(0.1, 3.0)
+ekAx.set_xlabel('Energy (GeV)')
+ekAx.set_title(r'Strauss\' code ($A_c < 0$, $\langle s \rangle = ' + str(round(propTime, 3)) + ' \pm '
+        + str(round(propTimeError, 3)) + '$ days)')
+thAx.set_xlim(0.0, 180.0)
+thAx.set_xlabel('Polar angle (degrees)')
+sAx.set_xlim(0.0, 1000.0)
+sAx.set_xlabel('Propagation time (days)')
 
-    for traj in trajectories:
-        # Make sure the trajectory ended at the heliopause
-        if (traj.first.r >= 140.0):
-            fromGalaxy += 1
-
-            # Check whether the trajectory is above or below the specified cut and fill the correct lists.
-            if (True):
-                thBelow.append(traj.first.th * 180.0 / np.pi)
-                eBelow.append(traj.first.e)
-                tBelow.append(traj.last.t / (24.0 * 60.0 * 60.0))
-            else:
-                thAbove.append(traj.first.th * 180.0 / np.pi)
-                eAbove.append(traj.first.e)
-                tAbove.append(traj.last.t / (24.0 * 60.0 * 60.0))
-
-    # Create figure
-    fig = plt.figure()
-    polarAx = fig.add_subplot(1, 3, 1)
-    energyAx = fig.add_subplot(1, 3, 2)
-    propTimeAx = fig.add_subplot(1, 3, 3)
-
-    # Create histograms
-    polarAx.hist(thBelow, bins = np.arange(0.0, 180.0 + 6.0, 6.0), log = True)
-    energyAx.hist(eBelow, bins = np.arange(0.0, 3.0 + 0.1, 0.1), log = True)
-    # Propagation times
-    tWidth = 10.0
-    tMin = 0.0
-    tMax = 1000.0
-    propTimeAx.hist(tBelow, bins = np.arange(tMin, tMax, tWidth))
-    timeCounts, timeBins = np.histogram(tBelow + tAbove, bins = np.arange(tMin, tMax, tWidth))
-
-    # Get probabilities for each bin
-    timeProbs = timeCounts / (fromGalaxy + 0.0)
-    # Find centers of bins
-    timeBins = timeBins[1:]
-    timeBins -= tWidth / 2.0
-    # Get expected propagation time
-    propTime = sum(t * p for t, p in zip(timeBins, timeProbs))
-
-    # Find standard deviation of the mean to get error estimates
-    timeError = np.std([float(t) / (24.0 * 60.0 * 60.0) for t in tBelow + tAbove],
-            ddof = 1) / np.sqrt(fromGalaxy)
-
-    print("The expected propagation time is " + str(propTime) + " days")
-
-    # Set axes and labels
-    polarAx.set_ylim(0.9, 6000.0)
-    energyAx.set_ylim(0.9, 9000.0)
-    propTimeAx.set_ylim(0.9, 500.0)
-    polarAx.set_ylabel("Number of occurences")
-    energyAx.set_ylabel("Number of occurences")
-    propTimeAx.set_ylabel("Number of occurences")
-
-    energyAx.set_xlim(0.1, 3.0)
-    energyAx.set_xlabel("Energy (GeV)")
-
-    polarAx.set_xlim(0.0, 180.0)
-    polarAx.set_xlabel("Polar angle (degrees)")
-
-    propTimeAx.set_xlim(0.0, 1000.0)
-    propTimeAx.set_xlabel("Propagation time (days)")
-
-    plt.show()
+plt.show()
+#fig.savefig('../figures/' + fName + '.pdf')
 
 
